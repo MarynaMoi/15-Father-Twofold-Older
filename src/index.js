bfc394d696a56f1183c3e21cc7f40c3e0b805627
Maryna <marinakukusua@gmail.com>
2025-10-25 16:10:44 +0300

    Add grammatical form

--- src/index.js
+++ src/index.js
@@ -4,34 +4,69 @@
  *
  * @param {number} fatherAge - вік батька
  * @param {number} sonAge - вік сина
- * @returns через скільки років (або скільки років тому) батько буде (або був) удвічі старшим за сина.
+ * @returns {number} через скільки років (якщо додатнє число),
+ * або скільки років тому(якщо від'ємне число) батько буде (або був)
+ * удвічі старшим за сина. В разі введення некоректних данних -  NaN
  */
 
 const countTwofold = function (fatherAge, sonAge) {
   fatherAge = Math.floor(fatherAge);
   sonAge = Math.floor(sonAge);
-  console.log(`Сьогодні Батьку: ${fatherAge}, а Сину: ${sonAge}`);
-  if (fatherAge - sonAge < 15) {
-    return console.log(`Різниця в віці занадто мала`);
-  }
-  if (sonAge < 0) {
-    return console.log(`Вік персонажів не може бути від'ємним`);
+
+  if (fatherAge - sonAge < 15 || sonAge < 0) {
+    return NaN;
   }
 
   const years = fatherAge - 2 * sonAge;
-
-  if (years === 0) {
-    return console.log("Сьогодні Батьку вдвічі більше років, ніж Сину");
-  } else if (years > 0) {
-    return console.log(
-      `Через ${years} років Батьку буде вдвічі більше років, ніж Сину`
-    );
-  } else if (years < 0) {
-    return console.log(
-      `${years * -1} років назад Батьку було вдвічі більше років, ніж Сину`
-    );
-  }
+  return years;
 };
 
-countTwofold(100, 80);
+/**
+ *
+ * @param {number} years - повертається з функції countTwofold
+ * @returns {string} визначає граматичні форму слова "рік"
+ */
+const getWordYear = function (years) {
+  let wordYears = "років";
+  const yearsString = String(Math.abs(years));
+  console.log(yearsString);
+  if (years < 10 || years > 20) {
+    switch (yearsString[yearsString.length - 1]) {
+      case "1":
+        wordYears = "рік";
+        break;
+      case "2":
+      case "3":
+      case "4":
+        wordYears = "роки";
+        break;
+      default:
+        wordYears = "років";
+    }
+  }
+  return wordYears;
+};
 
+const fatherAge = 63;
+const sonAge = 20;
+
+console.log(`Сьогодні Батьку: ${fatherAge}, а Сину: ${sonAge}`);
+const years = countTwofold(fatherAge, sonAge);
+
+if (Number.isNaN(years)) {
+  console.log(" Дані введено некоректно");
+} else if (years === 0) {
+  console.log("Сьогодні Батьку вдвічі більше років, ніж Сину");
+} else {
+  if (years > 0) {
+    console.log(
+      `Через ${years} ${getWordYear(years)} Батько буде вдвічі старше за Сина`
+    );
+  } else if (years < 0) {
+    console.log(
+      `${Math.abs(years)} ${getWordYear(
+        years
+      )} тому Батько був  вдвічі старше за Сина`
+    );
+  }
+}
